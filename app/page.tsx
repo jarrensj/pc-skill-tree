a6f251102d5d0af9c8829a2b43caadbf56af1fee
Jarren San Jose <sanjose.jarren@gmail.com>
2025-08-02 00:42:27 -0700

    add a macbook dev skill tree button

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <main className="text-center">
       <h1 className="text-2xl font-bold mb-4">set up the setup</h1>
+      <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-8 rounded-full">
+        mac dev skill tree
+      </button>
     </main>
   );
 }
